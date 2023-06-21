51ebabdba8c5b6ae884a58ffdab1f454d2fdf916
Haein Lee <lhi3050@gmail.com>
2023-06-21 10:05:43 +0900

    update changes; added some accessibility features, don't know how it will work

--- App.js
+++ App.js
@@ -1,11 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, View, Image, Text, Pressable, Accessibility} from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image source={require('./assets/logo.png')} style={styles.logo} />
+      <Text style={styles.text}>iCANsee</Text>
+      <View style={styles.buttonContainer}>
+        <Pressable 
+        style={[styles.button, styles.buttonVisualImpairment]} 
+        accessibile = {true}
+        accessibilityLabel = "시각장애인 모드. 왼쪽을 누르세요.">
+          <Text style={styles.buttonText}>시각장애인</Text>
+        </Pressable>
+        <Pressable 
+        style={[styles.button, styles.buttonNonVisualImpairment]}
+        accessible = {true}
+        accessibile = {true}
+        accessibilityLabel = "비시각장애인 모드. 오른쪽을 누르세요.">
+          <Text style={styles.buttonText}>비시각장애인</Text>
+        </Pressable>
+      </View>
     </View>
   );
 }
@@ -13,8 +28,40 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'black',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  logo: {
+    width: 100,
+    height: 100,
+    resizeMode: 'contain',
+  },
+  text: {
+    marginTop: 20,
+    color: 'white',
+    fontSize: 24,
+  },
+  buttonContainer: {
+    marginTop: 30,
+    flexDirection: 'row',
+  },
+  button: {
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    borderRadius: 10,
+    marginRight: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: 'black',
+    fontSize: 16,
+  },
+  buttonVisualImpairment: {
+    backgroundColor: 'yellow',
+  },
+  buttonNonVisualImpairment: {
+    backgroundColor: 'white',
   },
 });
