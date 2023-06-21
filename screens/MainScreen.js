76e61aa1ec0e3ca64f42fce9b26d225fad36b964
Haein Lee <lhi3050@gmail.com>
2023-06-21 16:43:39 +0900

    pls work

---
+++ screens/MainScreen.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import { StyleSheet, View, Image, Text, Pressable } from 'react-native';
+
+export default function MainScreen() {
+  return (
+    <View style={styles.container}>
+      <Image source={require('../../assets/logo.png')} style={styles.logo} />
+      <Text style={styles.text}>iCANsee</Text>
+      <View style={styles.buttonContainer}>
+        <Pressable
+          style={[styles.button, styles.buttonVisualImpairment]}
+          accessible={true}
+          accessibilityLabel="시각장애인 모드."
+        >
+          <Text style={styles.buttonText}>시각장애인</Text>
+        </Pressable>
+        <Pressable
+          style={[styles.button, styles.buttonNonVisualImpairment]}
+          accessible={true}
+          accessibilityLabel="비시각장애인 모드."
+        >
+          <Text style={styles.buttonText}>비시각장애인</Text>
+        </Pressable>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'black',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  logo: {
+    width: 100,
+    height: 100,
+    resizeMode: 'contain',
+  },
+  text: {
+    marginTop: 20,
+    color: 'white',
+    fontSize: 24,
+  },
+  buttonContainer: {
+    marginTop: 30,
+    flexDirection: 'row',
+  },
+  button: {
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    borderRadius: 10,
+    marginRight: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: 'black',
+    fontSize: 16,
+  },
+  buttonVisualImpairment: {
+    backgroundColor: 'yellow',
+  },
+  buttonNonVisualImpairment: {
+    backgroundColor: 'white',
+  },
+});
