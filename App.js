787e19384b876d2b0c7439ab1a81b2e1e468973a
Haein Lee <lhi3050@gmail.com>
2023-06-22 10:44:20 +0900

    Removed headers

--- App.js
+++ App.js
@@ -11,8 +11,8 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Camera" component={CameraScreen} />
+        <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false }}/>
+        <Stack.Screen name="Camera" component={CameraScreen} options={{ headerShown: false }} />
         
       </Stack.Navigator>
     </NavigationContainer>
